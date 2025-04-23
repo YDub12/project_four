e95a3f5e1506c949d4bf9a18740d88e86c1c866d
YDub12 <ydubina@googlemail.com>
2025-04-23 18:55:38 +0100

    update static files access

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'restaurant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'booking' 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'booking', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
